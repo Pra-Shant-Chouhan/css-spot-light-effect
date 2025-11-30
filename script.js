4e5569b4d42964e65fc79304364d5a0d8f6392b4
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2025-11-30 12:52:03 +0530

    optimize that with  requestAnimationFrame instead of throttle.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-addEventListener("mousemove",(e)=>{
-    console.log(e.clientX, e.clientY)
-    document.body.style.setProperty("--x",e.clientX+"px")
-    document.body.style.setProperty("--y",e.clientY+"px")
+// addEventListener("mousemove",(e)=>{
+//     console.log(e.clientX, e.clientY)
+//     document.body.style.setProperty("--x",e.clientX+"px")
+//     document.body.style.setProperty("--y",e.clientY+"px")
+// })
+let ticking = false;
+window.addEventListener("mousemove", (e) => {
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            // console.log(e.clientX, e.clientY)
+            document.body.style.setProperty("--x", e.clientX + "px");
+            document.body.style.setProperty("--y", e.clientY + "px");
+            ticking = false
+        })
+        ticking = true
+    }
 })
